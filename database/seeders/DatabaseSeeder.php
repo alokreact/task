f782d7c836b018a45d654ace1cb65621acac56f0
alok <alok.n@technovert.com>
2024-02-19 15:57:47 +0530

    3

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Appointment::factory()->count(10)->create();
+        //Appointment::factory()->count(10)->create();
         Product::factory()->count(10)->create();
     }
 }
